df8ef27ae6f9c7871161f7b9243c01eca68413c3
Fatih <fatakhillah.khaqo@gmail.com>
2024-06-26 13:48:51 +0700

    Initial Commit

---
+++ CekrekMiniChallenge/CekrekMiniChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CekrekMiniChallengeApp.swift
+//  CekrekMiniChallenge
+//
+//  Created by Fatakhillah Khaqo on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CekrekMiniChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
