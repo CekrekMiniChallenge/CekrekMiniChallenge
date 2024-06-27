c3daa04c4e55eafa587ad5dba60d5effc77d9fdb
Nadya2012 <137778828+Nadya2012@users.noreply.github.com>
2024-06-27 15:30:11 +0700

    enum card

---
+++ CekrekMiniChallenge/Card.swift
@@ -0,0 +1,11 @@
+//
+//  Card.swift
+//  CekrekMiniChallenge
+//
+//  Created by Nadya Margareth Angkawijaya on 27/06/24.
+//
+
+import Foundation
+enum CardType{
+    case basic(title: String, description: String, imageName: String)
+}
